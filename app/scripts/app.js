bef6aabe19d475fd762c40be83ce68e3395f43ef
Juan Marin <juan@codecery.com>
2014-06-25 18:36:45 -0500

    Adds routes.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,40 +6,47 @@ angular.module('weddingAppApp', [
 ])
 .config(function ($stateProvider, $urlRouterProvider) {
   //delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  $urlRouterProvider.otherwise('/');
+
   $stateProvider
-  .state('/about', {
-    templateUrl: 'views/about.html',
-    controller: 'AboutCtrl'
-  })
-  .state('/reception', {
-    templateUrl: 'views/reception.html',
-    controller: 'ReceptionCtrl'
-  })
-  .state('/accommodations', {
-    templateUrl: 'views/accommodations.html',
-    controller: 'AccommodationsCtrl'
-  })
-  .state('/dresscode', {
-    templateUrl: 'views/dresscode.html',
-    controller: 'DresscodeCtrl'
-  })
-  .state('/transportation', {
-    templateUrl: 'views/transportation.html',
-    controller: 'TransportationCtrl'
-  })
-  .state('/rsvp', {
-    templateUrl: 'views/rsvp.html',
-    controller: 'RsvpCtrl'
-  })
-  .state('/checklist', {
-    templateUrl: 'views/checklist.html',
-    controller: 'ChecklistCtrl'
-  })
   .state('index', {
     url: '/',
     templateUrl: 'views/main.html',
     controller:'MainCtrl'
+  })
+  .state('/about', {
+    url: '/about',
+    templateUrl: 'views/about.html',
+    controller: 'AboutCtrl'
+  })
+  .state('/reception', {
+    url: '/reception',
+    templateUrl: 'views/reception.html',
+    controller: 'ReceptionCtrl'
+  })
+  .state('/accommodations', {
+    url: '/accommodations',
+    templateUrl: 'views/accommodations.html',
+    controller: 'AccommodationsCtrl'
+  })
+  .state('/dresscode', {
+    url: '/dresscode',
+    templateUrl: 'views/dresscode.html',
+    controller: 'DresscodeCtrl'
+  })
+  .state('/transportation', {
+    url: '/transportation',
+    templateUrl: 'views/transportation.html',
+    controller: 'TransportationCtrl'
+  })
+  .state('/rsvp', {
+    url: '/rsvp',
+    templateUrl: 'views/rsvp.html',
+    controller: 'RsvpCtrl'
+  })
+  .state('/checklist', {
+    url: '/checklist',
+    templateUrl: 'views/checklist.html',
+    controller: 'ChecklistCtrl'
   });
-
-  $urlRouterProvider.otherwise('/');
 });
